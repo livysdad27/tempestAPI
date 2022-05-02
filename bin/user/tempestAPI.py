749a4cd360a8cae48f723c55bd5e6d412aa8483d
Billy Jackson <livysdad27@gmail.com>
2022-05-01 17:57:45 -0700

    creating loop packets now

--- bin/user/tempestAPI.py
+++ bin/user/tempestAPI.py
@@ -12,12 +12,35 @@ tempest_endpoint='wss://ws.weatherflow.com/swd/data'
 websocket_uri=tempest_endpoint + '?api_key=' + personal_token
 
 def on_message(ws, message):
+    loop_packet = {}
+    mqtt_data = []
     print('=========================================')
     msg = json.loads(message)
-    if 'obs' in msg.keys():
-      print(msg['obs'])
+    if msg['type']=='obs_st':
+        mqtt_data = msg['obs'][0]
+        loop_packet['dateTime'] = mqtt_data[0]
+        loop_packet['usUnits'] = 16
+        loop_packet['outTemp'] = mqtt_data[7]
+        loop_packet['outHumidity'] = mqtt_data[8]
+        loop_packet['pressure'] = mqtt_data[6]
+        loop_packet['supplyVoltage'] = mqtt_data[16]
+        loop_packet['radiation'] = mqtt_data[11]
+        loop_packet['rain'] = mqtt_data[19]
+        loop_packet['UV'] = mqtt_data[10]
+        loop_packet['lightening_distance'] = mqtt_data[14]
+        loop_packet['lightening_strike_count'] = mqtt_data[15]
+    elif msg['type']=='rapid_wind':
+        mqtt_data = msg['ob']
+        loop_packet['dateTime'] = mqtt_data[0]
+        loop_packet['usUnits'] = 16
+        loop_packet['windSpeed'] = mqtt_data[1]
+        loop_packet['windDir'] = mqtt_data[2] 
+    elif msg['type']=='ack':
+        print(msg['id'])
     else:
         print(json.dumps(msg, indent=4))
+    if loop_packet != {}:
+        print(loop_packet)
     print('=========================================')
 
 
@@ -35,7 +58,7 @@ def on_open(ws):
     print("Opened connection")
     ws.send(('{"type":"listen_start", "device_id":' + tempest_device_ID + ',' + ' "id":"corrOpenOb"}'))
     ws.send(('{"type":"listen_rapid_start", "device_id":' + tempest_device_ID + ',' + ' "id":"corrOpenWind"}'))
-    ws.send(('{"type":"listen_start_events", "station_id":' + tempest_station_ID + ',' + ' "id":"corrOpenStation"}')) 
+    #ws.send(('{"type":"listen_start_events", "station_id":' + tempest_station_ID + ',' + ' "id":"corrOpenStation"}')) 
 
 
 if __name__ == "__main__":
