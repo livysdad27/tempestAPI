5990fcbf8c14fed3b8e4d2eab5fd40845eae6fa2
Billy Jackson <livysdad27@gmail.com>
2022-05-03 18:43:09 -0700

    moved to the abstract class, throwing type errors

--- bin/user/tempestAPI.py
+++ bin/user/tempestAPI.py
@@ -3,71 +3,91 @@ import _thread
 import time
 import rel
 import json
+import weewx
+import getopt
 
-personal_token = ''
-tempest_device_ID = '195926'
-tempest_station_ID = '72866'
-tempest_endpoint='wss://ws.weatherflow.com/swd/data'
-websocket_uri=tempest_endpoint + '?api_key=' + personal_token
+DRIVER_VERSION = "0.8"
+HARDWARE_NAME = "Weatherflow Tempest"
+DRIVER_NAME = "tempestAPI"
 
-def on_message(ws, message):
-    loop_packet = {}
-    mqtt_data = []
-    print('=========================================')
-    msg = json.loads(message)
-    if msg['type']=='obs_st':
-        mqtt_data = msg['obs'][0]
-        loop_packet['dateTime'] = mqtt_data[0]
-        loop_packet['usUnits'] = 16
-        loop_packet['outTemp'] = mqtt_data[7]
-        loop_packet['outHumidity'] = mqtt_data[8]
-        loop_packet['pressure'] = mqtt_data[6]
-        loop_packet['supplyVoltage'] = mqtt_data[16]
-        loop_packet['radiation'] = mqtt_data[11]
-        loop_packet['rain'] = mqtt_data[19]
-        loop_packet['UV'] = mqtt_data[10]
-        loop_packet['lightening_distance'] = mqtt_data[14]
-        loop_packet['lightening_strike_count'] = mqtt_data[15]
-    elif msg['type']=='rapid_wind':
-        mqtt_data = msg['ob']
-        loop_packet['dateTime'] = mqtt_data[0]
-        loop_packet['usUnits'] = 16
-        loop_packet['windSpeed'] = mqtt_data[1]
-        loop_packet['windDir'] = mqtt_data[2] 
-    elif msg['type']=='ack':
-        print(msg['id'])
-    else:
-        print(json.dumps(msg, indent=4))
-    if loop_packet != {}:
-        print(loop_packet)
-    print('=========================================')
+def loader(config_dict, engine):
+    return tempestAPI(**config_dict[DRIVER_NAME])
+
+def logmsg(level, msg):
+    syslog.syslog(level, 'tempestAPI: %s: %s' %
+                  (threading.currentThread().getName(), msg))
+
+def logdbg(msg):
+    logmsg(syslog.LOG_DEBUG, msg)
+
+def loginf(msg):
+    logmsg(syslog.LOG_INFO, msg)
+
+def logerr(msg):
+    logmsg(syslog.LOG_ERR, msg)
 
 
-def on_error(ws, error):
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    print(error)
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+class tempestAPI(weewx.drivers.AbstractDevice):
 
-def on_close(ws, close_status_code, close_msg):
-    print('-------------------------------------')
-    print("### closed ###")
-    print('-------------------------------------')
+    def __init__(self, **cfg_dict):
+        self._personal_token = str(cfg_dict.get('personal_token'))
+        self._tempest_device_ID = str(cfg_dict.get('tempest_device_ID'))
+        self._tempest_station_ID = str(cfg_dict.get('tempest_station_ID'))
+        self._tempest_websocket_endpoint = str(cfg_dict.get('weatherflow_websocket_URI'))
 
-def on_open(ws):
-    print("Opened connection")
-    ws.send(('{"type":"listen_start", "device_id":' + tempest_device_ID + ',' + ' "id":"corrOpenOb"}'))
-    ws.send(('{"type":"listen_rapid_start", "device_id":' + tempest_device_ID + ',' + ' "id":"corrOpenWind"}'))
-    ws.send(('{"type":"listen_start_events", "station_id":' + tempest_station_ID + ',' + ' "id":"corrOpenStation"}')) 
+    def on_message(self, ws, message):
+        loop_packet = {}
+        mqtt_data = []
+        msg = json.loads(message)
+        if msg['type']=='obs_st':
+            mqtt_data = msg['obs'][0]
+            loop_packet['dateTime'] = mqtt_data[0]
+            loop_packet['usUnits'] = weewx.METRIXWX
+            loop_packet['outTemp'] = mqtt_data[7]
+            loop_packet['outHumidity'] = mqtt_data[8]
+            loop_packet['pressure'] = mqtt_data[6]
+            loop_packet['supplyVoltage'] = mqtt_data[16]
+            loop_packet['radiation'] = mqtt_data[11]
+            loop_packet['rain'] = mqtt_data[19]
+            loop_packet['UV'] = mqtt_data[10]
+            loop_packet['lightening_distance'] = mqtt_data[14]
+            loop_packet['lightening_strike_count'] = mqtt_data[15]
+        elif msg['type']=='rapid_wind':
+            mqtt_data = msg['ob']
+            loop_packet['dateTime'] = mqtt_data[0]
+            loop_packet['usUnits'] = weewx.METRIXWX
+            loop_packet['windSpeed'] = mqtt_data[1]
+            loop_packet['windDir'] = mqtt_data[2] 
+        #elif msg['type']=='ack':
+        #    print('ACK-> ' + msg['id'])
+        #else:
+        #    print(json.dumps(msg))
+        if loop_packet != {}:
+            print(loop_packet)
 
+    def on_error(self, ws, error):
+        print(error)
 
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(websocket_uri,
-                              on_open=on_open,
-                              on_message=on_message,
-                              on_error=on_error,
-                              on_close=on_close)
+    def on_close(self, ws, close_status_code, close_msg):
+        print('!!!! Connection Closed !!!!')
 
-    ws.run_forever(dispatcher=rel)  # Set dispatcher to automatic reconnection
-    rel.signal(2, rel.abort)  # Keyboard Interrupt
-    rel.dispatch()
+    def on_open(self, ws):
+        ws.send(('{"type":"listen_start", "device_id":' + self._tempest_device_ID + ',' + ' "id":"listen_start"}'))
+        ws.send(('{"type":"listen_rapid_start", "device_id":' + self._tempest_device_ID + ',' + ' "id":"rapid_wind"}'))
+        #ws.send(('{"type":"listen_start_events", "station_id":' + self._tempest_station_ID + ',' + ' "id":"listen_start_events"}')) 
+
+    def hardware_name(self):
+        return HARDWARE_NAME
+    
+    def genLoopPackets(self):
+        websocket_uri=self._tempest_websocket_endpoint + '?api_key=' + self._personal_token
+        websocket.enableTrace(True)
+        ws = websocket.WebSocketApp(websocket_uri,
+                                on_open=self.on_open,
+                                on_message=self.on_message,
+                                on_error=self.on_error,
+                                on_close=self.on_close)
+
+        ws.run_forever(dispatcher=rel)  # Set dispatcher to automatic reconnection
+        rel.signal(2, rel.abort)  # Keyboard Interrupt
+        rel.dispatch()
