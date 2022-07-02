59784d5b753e84b4118bbaa9e255d154407163fa
Billy <livysdad27@users.noreply.github.com>
2022-07-01 22:10:59 -0700

    simple config stanza

--- install.py
+++ install.py
@@ -12,13 +12,12 @@ class tempestAPIInstaller(ExtensionInstaller):
             author="Billy Jackson",
             author_email="livysdad27@gmail.com",
             files=[('bin/user', ['bin/user/tempestAPI.py'])],
-            config={
+            config=
                 'tempestAPI': {
                     'driver' : 'bin.user.tempestAPI',
-                    'personal_token': 'REPLACE WITH YOUR TOKEN FROM THE TEMPEST AUTHORIZATIONS PAGE',
-                    'tempest_device_id': 'REPLACE WITH YOUR TEMPEST DEVICE ID',
-                    'tempest_rest_endpoint': 'https://swd.weatherflow.com/swd/rest/observations/device/',
+                    'personal_token': '"REPLACE WITH YOUR TOKEN FROM THE TEMPEST AUTHORIZATIONS PAGE"',
+                    'tempest_device_id': '"REPLACE WITH YOUR TEMPEST DEVICE ID"',
+                    'tempest_rest_endpoint': 'https://swd.weatherflow.com/swd/rest/observations/device/',                        
                     'rest_sleep_interval': '20'
-                    }
                 },
             )
